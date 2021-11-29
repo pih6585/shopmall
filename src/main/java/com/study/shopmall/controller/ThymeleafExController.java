ef410aba90bad1864d9e256d5a97220e06ad20e0
pih6585 <pih6585@naver.com>
2021-11-29 12:22:43 +0900

    feat : footer, header layout 추가

--- src/main/java/com/study/shopmall/controller/ThymeleafExController.java
+++ src/main/java/com/study/shopmall/controller/ThymeleafExController.java
@@ -77,4 +77,9 @@ public class ThymeleafExController {
 		return "thymeleafEx/thymeleafEx06";
 	}
 
+	@GetMapping(value = "/ex07")
+	public String thymeleafExample07() {
+		return "thymeleafEx/thymeleafEx07";
+	}
+
 }
